70e885999367bda49b0293177e8fbe463da8fcf8
Alejandro Cordova <alejandrocordova198@gmail.com>
2021-02-23 13:18:13 -0600

    Agregado Componente Agregar y comenzando su funcionalidad

---
+++ src/app/models/lista.model.ts
@@ -0,0 +1,19 @@
+import { ListaItem } from './lista-item.model.';
+
+export class Lista{
+    id: number;
+    titulo: string;
+    creadaEn: Date;
+    terminadaEn:Date;
+    terminada:boolean;
+    items: Lista[];
+
+    constructor(titulo:string, ){
+        this.id= new Date().getTime();
+
+        this.titulo=titulo;
+        this.creadaEn = new Date();
+        this.terminada=false;
+        this.items = [];
+    }
+}
